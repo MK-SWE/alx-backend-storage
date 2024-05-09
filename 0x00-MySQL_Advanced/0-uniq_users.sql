2b1ea46b016dd63f00770d7b8de6389212f659a0
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-09 14:39:49 -0700

    create table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Create table users if now exists
 CREATE TABLE IF NOT EXISTS users(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255) NOT NULL
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email varchar(255) NOT NULL UNIQUE,
+  name varchar(255) NOT NULL
 );
