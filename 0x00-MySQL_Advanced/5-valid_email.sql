c1278971b934bdfe9d6c0a4064b82d6c2d39e451
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-09 18:36:54 -0700

    trigger with condition

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,7 +4,9 @@ CREATE TRIGGER email_validation
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = 0;
-    END IF;
+  IF NEW.email <> OLD.email THEN
+    SET NEW.valid_email = 0;
+  END IF;
 END;
+-- Add the delimiter statement
+DELIMITER ;
