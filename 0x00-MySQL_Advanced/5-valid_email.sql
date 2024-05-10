e9237ab5c6d7341b67a18e70015fb1c95cc181c9
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-09 18:38:24 -0700

    trigger with condition

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,5 +1,6 @@
 -- A SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed.
 
+DELIMITER $$;
 CREATE TRIGGER email_validation
 BEFORE UPDATE ON users
 FOR EACH ROW
@@ -7,6 +8,5 @@ BEGIN
   IF NEW.email <> OLD.email THEN
     SET NEW.valid_email = 0;
   END IF;
-END;
--- Add the delimiter statement
-DELIMITER ;
+END; $$
+delimiter;
