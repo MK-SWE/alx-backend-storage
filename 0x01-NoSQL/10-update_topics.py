1add84fa567ba48a6f59c55226ec5d90975a8138
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-14 19:59:57 -0700

    pymongo

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+  function that changes all topics of a school document based on the name
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+        return the id of new inserted
+    """
+    pass
