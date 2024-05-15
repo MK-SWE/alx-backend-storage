20ce168bcaf80131ac86df32155fe7641e65b36e
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-14 19:57:27 -0700

    pymongo

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,10 +5,8 @@
 import pymongo
 
 
-def insert_school(mongo_collection, **kwargs) -> int | None:
+def insert_school(mongo_collection, **kwargs) -> int :
     """
         return the id of new inserted
     """
-    if(not mongo_collection):
-        return None
     return mongo_collection.insert(kwargs)
